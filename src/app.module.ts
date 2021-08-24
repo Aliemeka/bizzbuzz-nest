f0881082e02def8895eb4951046f374c7163a16e
Emeka Allison <emekaallison4@gmail.com>
2021-08-24 05:46:08 +0100

    Feat: Integrated TypeOrm for PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeormConfig } from './config/typeorm.config';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeormConfig)],
   controllers: [AppController, PostsController],
   providers: [AppService, PostsService],
 })
